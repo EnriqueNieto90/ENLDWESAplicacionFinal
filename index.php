6eab5a9afb33a84fbb1eb6452fe8214e37f00380
EnriqueNieto90 <enrikeniet90@gmail.com>
2026-01-27 01:51:33 +0100

    cambio de confDBPDO para desarrollo

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 // CARGA DE CONFIGURACIONES
 require_once 'config/confAPP.php';
-require_once 'config/confDBPDO.php';
+require_once 'config/EDconfDBPDO.php';
 
 // INICIAR O RECUPERAR SESIÓN
 session_start();
