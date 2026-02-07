dffb46e4028f4be0477b4d8ed0fde13b2dad7ce4
EnriqueNieto90 <enrikeniet90@gmail.com>
2026-02-07 18:07:59 +0100

    fix: cambios en borrar usuario, poner campos para ver qué usuario estamos borrando

--- view/vBorrarCuenta.php
+++ view/vBorrarCuenta.php
@@ -1,35 +1,78 @@
 <?php
 /**
  * @author: Enrique Nieto Lorenzo
- * @since: 02/02/2026
+ * @since: 07/02/2026
  * @description: Vista de confirmación de borrado de cuenta.
  */
 ?>
 <main>
-    <div class="card-central" style="max-width: 500px; text-align: center;">
+    <div class="card-central card-media">
         
-        <div class="icono-alerta">
-            <i class="fa-solid fa-triangle-exclamation"></i>
+        <div class="seccion-titulo encabezado-alerta">
+            <div class="icono-alerta">
+                <i class="fa-solid fa-triangle-exclamation"></i>
+            </div>
+
+            <h2 class="titulo-login titulo-peligro">¿Eliminar tu cuenta?</h2>
+            
+            <p class="texto-advertencia">
+                Estás a punto de eliminar tu usuario <strong>permanentemente</strong>.
+                <span class="subtexto-advertencia">
+                    Todos tus datos se perderán y no podrás recuperarlos.
+                </span>
+            </p>
         </div>
 
-        <h2 class="titulo-login" style="color: #d13438;">¿Eliminar cuenta?</h2>
-        
-        <p style="margin: 20px 0; font-size: 1.1em; color: #333;">
-            Estás a punto de eliminar tu cuenta <strong>permanentemente</strong>. 
-            <br>
-            <span style="font-size: 0.9em; color: #666;">Todos tus datos se perderán y no podrás recuperarlos.</span>
-        </p>
-
-        <form action="index.php" method="post" class="form-botones-borrar">
+        <form action="index.php" method="post">
             
-            <button type="submit" name="eliminar" class="btn-primary btn-peligro">
-                Aceptar
-            </button>
+            <div class="fila-flex">
+                <div class="grupo-input col-flex">
+                    <label class="label-form">Usuario</label>
+                    <input type="text" class="input-microsoft" disabled readonly
+                           value="<?php echo $avBorrarCuenta['codUsuario']; ?>">
+                </div>
+                
+                <div class="grupo-input col-flex">
+                    <label class="label-form">Perfil</label>
+                    <input type="text" class="input-microsoft" disabled readonly
+                           value="<?php echo $avBorrarCuenta['perfil']; ?>">
+                </div>
+            </div>
 
-            <button type="submit" name="cancelar" class="btn-primary btn-gris">
-                Cancelar
-            </button>
-            
+            <div class="grupo-input">
+                <label class="label-form">Nombre y Apellidos</label>
+                <input type="text" class="input-microsoft" disabled readonly
+                       value="<?php echo $avBorrarCuenta['descUsuario']; ?>">
+            </div>
+
+            <div class="fila-flex">
+                <div class="grupo-input col-flex">
+                    <label class="label-form">Nº Conexiones</label>
+                    <input type="text" class="input-microsoft" disabled readonly
+                           value="<?php echo $avBorrarCuenta['numConexiones']; ?>">
+                </div>
+
+                <div class="grupo-input col-flex">
+                    <label class="label-form">Última Conexión</label>
+                    <input type="text" class="input-microsoft" disabled readonly
+                           value="<?php echo $avBorrarCuenta['ultimaConexion']; ?>">
+                </div>
+            </div>
+
+            <hr class="separador-horizontal">
+
+            <div class="grupo-botones grupo-botones-vertical">
+                
+                <button type="submit" name="eliminar" class="btn-primary btn-peligro btn-full">
+                    Aceptar
+                </button>
+
+                <button type="submit" name="cancelar" class="btn-primary btn-gris btn-full">
+                    Cancelar
+                </button>
+                
+            </div>
+
         </form>
 
     </div>
